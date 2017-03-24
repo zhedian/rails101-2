6eb0336e0f5ca6e38e2591c5a6d07777beb8ac16
zhedian <cooldeer@126.com>
2017-03-24 22:47:43 +0800

    render partial with collection

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -5,15 +5,5 @@ class Account::PostsController < ApplicationController
     @posts = current_user.posts
   end
 
-  def edit
-    @group = Group.find(params[:group_id])
-    @post = Post.find(params[:post_id])
-  end
 
-  def destroy
-    @group = Group.find(params[:group_id])
-    @post = Post.find(params[:post_id])
-    @post.destroy
-    redirect_to posts_path
-  end
 end
